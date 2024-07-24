a1a9533250e15309702f443b042320de4abc033b
youssef nagib <youssefnagib14@yahoo.com>
2024-07-24 03:28:39 +0300

    fix

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines square class"""
+
+class Square():
+    """Square class"""
+    
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        """init square class"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def permiter_of_my_square(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """Returns a string representation of the square"""
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+    """main function"""
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
